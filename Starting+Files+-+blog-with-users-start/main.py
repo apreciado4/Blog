1c2d3799ae98f682d5077d65dbfce95bab19e22c
Andres Preciado <apreciado4@live.com>
2022-03-17 14:05:20 -0700

    Cleanup Packages

--- Starting+Files+-+blog-with-users-start/main.py
+++ Starting+Files+-+blog-with-users-start/main.py
@@ -1,14 +1,10 @@
 from functools import wraps
-from urllib import request
 from flask import Flask, render_template, redirect, url_for, flash, abort
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from datetime import date
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import ForeignKey
-from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
